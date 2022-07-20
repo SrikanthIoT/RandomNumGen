4bce3bb2b978af2d5d25c7e14e223f28e6e6c00f
srikantht2309 <srikantht2309@gmail.com>
2022-07-20 10:49:01 -0500

    Implementing random numbers logic

--- RandomNumGen/Program.cs
+++ RandomNumGen/Program.cs
@@ -4,6 +4,10 @@ public class Program
 {
     public static void Main(string[] args)
     {
-
+        const int listSize = 10000;
+        var rnd = new Random();
+        var randomList = Enumerable.Range(1, listSize).OrderBy(e => rnd.Next()).ToList();
+        var listOutput = string.Join(", ", randomList);
+        Console.WriteLine(listOutput);
     }
 }
